1a5d2ecafaa6a2e21a86c13de446a680f24f5c45
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-17 21:24:44 +0530

    added the rate limiter middleware

--- src/app.js
+++ src/app.js
@@ -20,11 +20,13 @@ const limiter = rateLimit({
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   message: "Too many requests from this IP, please try again after 15 minutes",});
 
+  app.use(limiter);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(middleware);
 app.use(cookieParser());
+
 
 app.get("/", (req, res) => {
   res.status(200).send(` ${process.pid} Hello World`);
