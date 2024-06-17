9fae563c63e28ae06f8c16357c32733028eef25f
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-17 18:17:40 +0530

    added rate limit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { middleware } from "./utils/prometheus.js";
 import router from "./routes/index.route.js";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 
@@ -12,6 +13,13 @@ app.use(
     credentials: true,
   })
 );
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  message: "Too many requests from this IP, please try again after 15 minutes",});
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
