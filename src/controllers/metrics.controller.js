19bfd6085a2aca9db86dff4cc8a0046f268693a5
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-19 21:00:18 +0530

    removed the password stuff from the prometheus route:

--- src/controllers/metrics.controller.js
+++ src/controllers/metrics.controller.js
@@ -1,12 +1,8 @@
-import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { prometheus } from "../utils/prometheus.js";
 
 const metrics = asyncHandler(async (req, res) => {
-  if(req.headers.metricspass != process.env.METRICS_PASSWORD){
-    return res.status(401).json(new ApiError(401,"Unauthorized"));
-   
-  }
+ 
     res.set('Content-Type', prometheus.register.contentType);
     res.status(200).send(await prometheus.register.metrics());
   });
